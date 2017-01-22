e7a482eac024f5a13893f489321d7bfc8019a179
Timothy Reed <tReed@catalystsolves.com>
2017-01-21 21:42:53 -0500

    added environment variables to encapsulate sensitive config data

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,11 +3,11 @@ import firebase from 'firebase';
 try {
     //Initialize Firebase
     var config = {
-        apiKey: "AIzaSyBX43XxDp9Gkku4QZnfdH-9YTD6f4IjPao",
-        authDomain: "timtodoapp.firebaseapp.com",
-        databaseURL: "https://timtodoapp.firebaseio.com",
-        storageBucket: "timtodoapp.appspot.com",
-        messagingSenderId: "722800151438"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
     firebase.initializeApp(config);
 
